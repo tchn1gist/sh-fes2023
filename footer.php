95113b3ff1f9b114585c4bdea4e536daac021e5b
tchn1gist <105536970+tchn1gist@users.noreply.github.com>
2022-09-26 17:01:51 +0900

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,67 @@
+        <!-- CSS only -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <!-- JavaScript Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        <script>
+            setTimeout( ()=> document.body.style.overflow = "visible", 10000 );
+              // Add slideDown animation to Bootstrap dropdown when expanding.
+            function jump_to_club () {
+                location.href = "club.html"
+            }
+
+        </script>
+        <link rel="stylesheet" href="style.css">
+<div class="footer05">
+            <div class="wrap">
+
+             <div class="box">
+               <h3></h3>
+               <ul>
+                 <li><a href="#"></a></li>
+                 <li><a href="#"></a></li>
+                 <li><a href="#"></a></li>
+                 <li><a href="#"></a></li>
+                 <li><a href="#"></a></li>
+               </ul>
+             </div>
+
+             <div class="box">
+              <h3></h3>
+              <ul>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+              </ul>
+            </div>
+
+            <div class="box">
+              <h3></h3>
+              <ul>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+              </ul>
+            </div>
+
+            <div class="box">
+              <h3></h3>
+              <ul>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+              </ul>
+            </div>
+             <button type="button" class="btn btn-outline-dark">開発者画面</button>
+             
+             <p class="copyright">
+               ©︎<a href="https://sakaehigashi.ed.jp">Sakaehigashi Junior / Senior High School</a> & '23栄東祭 All Rights Reserved.
+             </p>
+            </div>
+           </div>
+        <script src="/docs/5.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
